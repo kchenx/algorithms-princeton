12ce5f339bed59250e5698488986525b98f02590
Kevin Chen <kevin.chen.krc@gmail.com>
2020-01-12 02:01:25 -0500

    Fix backwash in percolation

--- week1-percolation/src/Percolation.java
+++ week1-percolation/src/Percolation.java
@@ -4,7 +4,7 @@
  *
  *  This program implements a class which models the percolation problem:
  *
- *    - A percolation system is an n-by-n grid of sites
+ *    - A percolation system is an `n`-by-`n` grid of sites
  *    - Each site is either "open" or "blocked"
  *    - A "full" site is an open site that can be connected to an open site 
  *      in the top row via a chain of neighboring (left, right, up, down) 
@@ -12,6 +12,20 @@
  *    - A system "percolates" iff there is a full site in the bottom row.
  *      In other words, a system percolates iff we fill all open sites connected 
  *      to the top row and that process fills some open site on the bottom row. 
+ *      
+ *  The program does this with Union-Find data structures, modeling each site
+ *  as an element. When a site is opened, then it is union-ed with its neighboring
+ *  sites, and it is additionally union-ed to the top virtual node if it is in
+ *  the first row, and the bottom virtual node if it is in the bottom row.
+ *  
+ *  Thus, the system percolates iff the top and bottom virtual nodes are connected.
+ *  Note that we keep a separate union-find structure to track full-ness because
+ *  once the system percolates, there may be "backwash": if the bottom virtual node
+ *  is connected to open sites that do not reach the top, since they are part of the
+ *  same set as the bottom virtual node, which percolates, it will seem like they
+ *  are full (have a path to the top row) when they actually do not. In our other
+ *  union-find structure to track fullness, we do not union with the bottom
+ *  virtual node to solve the problem.
  *
  ******************************************************************************/
 
@@ -19,37 +33,42 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
-    private final boolean[][] m;
-    private final int n;
-    private final WeightedQuickUnionUF uf;
+    // union-find structure to see if system percolates
+    private WeightedQuickUnionUF uf;
+    
+    // union-find structure to see if node full
+    private WeightedQuickUnionUF ufNoBackwash;
+    
+    // virtual node to be attached to top sites
+    private final int topVirtualNode;
+    
+    // virtual node to be attached to bottom sites
+    private final int bottomVirtualNode;
+    
+    // grid to check if site is open: a site is open iff true
+    private boolean[][] m;
+    
+    // number of sites open
     private int nopen = 0;
+    
+    // size of grid: percolation system is `n`-by-`n`
+    private final int n;
+    
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(final int n) {
         if (n <= 0) {
             throw new IllegalArgumentException("n must be positive");
         }
-
-        m = new boolean[n + 1][n + 1];
-        for (int i = 0; i < n + 1; i++) {
-            for (int j = 0; j < n + 1; j++) {
-                m[i][j] = false;
-            }
-        }
-
-        this.n = n;
-
+        
+        ufNoBackwash = new WeightedQuickUnionUF(n * n + 2);
         uf = new WeightedQuickUnionUF(n * n + 2);
 
-        // connect top row to source
-        for (int i = 0; i < n; i++) {
-            uf.union(i, n * n);
-        }
+        m = new boolean[n + 1][n + 1];
 
-        // connect bottom row to sink
-        for (int i = n * (n - 1); i < n * n; i++) {
-            uf.union(i, n * n + 1);
-        }
+        this.n = n;
+        topVirtualNode = n * n;
+        bottomVirtualNode = n * n + 1;
     }
 
     // opens the site (row, col) if it is not open already
@@ -60,24 +79,33 @@ public class Percolation {
 
         m[row][col] = true;
         nopen++;
-
+        
+        // connect to adjacent nodes if they exist
         if (row > 1 && isOpen(row - 1, col)) {
-            uf.union(xyTo1D(row - 1, col), xyTo1D(row, col));
+            unionToBoth(xyTo1D(row - 1, col), xyTo1D(row, col));
         }
         if (col > 1 && isOpen(row, col - 1)) {
-            uf.union(xyTo1D(row, col - 1), xyTo1D(row, col));
+            unionToBoth(xyTo1D(row, col - 1), xyTo1D(row, col));
         }
         if (row < n && isOpen(row + 1, col)) {
-            uf.union(xyTo1D(row + 1, col), xyTo1D(row, col));
+            unionToBoth(xyTo1D(row + 1, col), xyTo1D(row, col));
         }
         if (col < n && isOpen(row, col + 1)) {
-            uf.union(xyTo1D(row, col + 1), xyTo1D(row, col));
+            unionToBoth(xyTo1D(row, col + 1), xyTo1D(row, col));
+        }
+        
+        // connect to virtual nodes if appropriate
+        if (row == 1) {
+            unionToBoth(xyTo1D(row, col), topVirtualNode);
+        }
+        if (row == n) {
+            uf.union(xyTo1D(row, col), bottomVirtualNode);
         }
     }
 
     // is the site (row, col) open?
     public boolean isOpen(final int row, final int col) {
-        if (!validateIndices(row, col)) {
+        if (!inRange(row, col)) {
             throw new IllegalArgumentException("Out of bounds");
         }
 
@@ -86,11 +114,11 @@ public class Percolation {
 
     // is the site (row, col) full?
     public boolean isFull(final int row, final int col) {
-        if (!validateIndices(row, col)) {
+        if (!inRange(row, col)) {
             throw new IllegalArgumentException("Out of bounds");
         }
 
-        return (isOpen(row, col)) && (uf.find(xyTo1D(row, col)) == uf.find(n * n));
+        return (isOpen(row, col)) && (ufNoBackwash.find(xyTo1D(row, col)) == ufNoBackwash.find(topVirtualNode));
     }
 
     // returns the number of open sites
@@ -103,7 +131,7 @@ public class Percolation {
         if (n == 1) {
             return isOpen(1, 1);
         }
-        return uf.find(n * n) == uf.find(n * n + 1);
+        return uf.find(topVirtualNode) == uf.find(bottomVirtualNode);
     }
 
     // converts (row, col) to index for Union-Find
@@ -112,8 +140,14 @@ public class Percolation {
     }
 
     // returns true iff indices are valid
-    private boolean validateIndices(final int row, final int col) {
+    private boolean inRange(final int row, final int col) {
         return row > 0 && col > 0 && row <= n && col <= n;
+    }
+    
+    // unions to both `uf` and `ufNoBackwash` structures
+    private void unionToBoth(int p, int q) {
+        uf.union(p, q);
+        ufNoBackwash.union(p, q);
     }
 
     // test client (optional)
